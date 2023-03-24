c8d44c360b18290cbb397cae1c812a3593fce55e
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-24 11:55:11 +0100

    eliminar fallos por no estar definidas

--- ejercicio 1/libro.py
+++ ejercicio 1/libro.py
@@ -1,18 +1,33 @@
 class libro:
     def libro(self):
         self.autor = autor
+        autor = input("Introduce el autor del libro: ")
         self.titulo = titulo
+        titulo = input("Introduce el titulo del libro: ")
         self.editorial = editorial
+        editorial = input("Introduce la editorial del libro: ")
         self.paginas = paginas
+        paginas = input("Introduce el numero de paginas del libro: ")
         self.precio = precio
+        precio = input("Introduce el precio del libro: ")
         self.isbn = isbn
+        isbn = input("Introduce el isbn del libro: ")
         self.fecha_publicacion = fecha_publicacion
+        fecha_publicacion = input("Introduce la fecha de publicacion del libro: ")
         self.categoria = categoria
+        categoria = input("Introduce la categoria del libro: ")
         self.idioma = idioma
+        idioma = input("Introduce el idioma del libro: ")
         self.pais = pais
+        pais = input("Introduce el pais del libro: ")
         self.formato = formato
+        formato = input("Introduce el formato del libro: ")
         self.peso = peso
+        peso = input("Introduce el peso del libro: ")
         self.dimensiones = dimensiones
+        dimensiones = input("Introduce las dimensiones del libro: ")
         self.descripcion = descripcion
+        descripcion = input("Introduce la descripcion del libro: ")
         self.portada = portada
+        portada = input("Introduce la portada del libro: ")
         #etc
