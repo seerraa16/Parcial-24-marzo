c069c5c78f5b9b2acdda138e60f0ac010e03c0f0
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-24 12:06:42 +0100

    super

--- ejercicio 2/definirclase.py
+++ ejercicio 2/definirclase.py
@@ -1,12 +1,13 @@
 class animal:
-    def __init__(Caracteristica, Animal):
-     print(f"El {Animal}, es: {Caracteristica}")
+    def __init__(especie, Animal):
+     print(f"El {Animal}, es: {especie}")
      #los atributos
-        self.Caracteristica = Caracteristica
+        self.especie = especie
         self.Animal = Animal
+    super().__init__(especie, Animal)
 pollo = animal("Oviparo", "Pollo")
 gato = animal("Mamifero", "Gato")
 ornitorrinco = animal("Mamifero y oviparo a la vez", "Ornitorrinco")
-print (f"El {gato.Animal}, es: {gato.Caracteristica}")
-print (f"El {ornitorrinco.Animal}, es: {ornitorrinco.Caracteristica}")
-print (f"El {pollo.Animal}, es: {pollo.Caracteristica}")
+print (f"El {gato.Animal}, es: {gato.especie}")
+print (f"El {ornitorrinco.Animal}, es: {ornitorrinco.especie}")
+print (f"El {pollo.Animal}, es: {pollo.especie}")
