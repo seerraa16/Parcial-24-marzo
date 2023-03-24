a2c9db3e56f6af4270978dd353b131eef0b8b870
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-24 11:19:40 +0100

    clase de los animales

---
+++ ejercicio 2/definirclase.py
@@ -0,0 +1,12 @@
+Class animal:
+    def __init__(Caracteristica, Animal):
+     print(f"El {Animal}, es: {Caracteristica}")
+     #los atributos
+        self.Caracteristica = Caracteristica
+        self.Animal = Animal
+pollo = animal("Oviparo", "Pollo")
+gato = animal("Mamifero", "Gato")
+ornitorrinco = animal("Mamifero y oviparo a la vez", "Ornitorrinco")ç
+print (f"El {gato.Animal}, es: {gato.Caracteristica}")
+print (f"El {ornitorrinco.Animal}, es: {ornitorrinco.Caracteristica}")
+print (f"El {pollo.Animal}, es: {pollo.Caracteristica}")
