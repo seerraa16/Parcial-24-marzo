8bce5ff86ac42636209237479f90e6cebf325295
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-24 11:52:33 +0100

    clase libro completa

---
+++ ejercicio 1/libro.py
@@ -0,0 +1,18 @@
+class libro:
+    def libro(self):
+        self.autor = autor
+        self.titulo = titulo
+        self.editorial = editorial
+        self.paginas = paginas
+        self.precio = precio
+        self.isbn = isbn
+        self.fecha_publicacion = fecha_publicacion
+        self.categoria = categoria
+        self.idioma = idioma
+        self.pais = pais
+        self.formato = formato
+        self.peso = peso
+        self.dimensiones = dimensiones
+        self.descripcion = descripcion
+        self.portada = portada
+        #etc
